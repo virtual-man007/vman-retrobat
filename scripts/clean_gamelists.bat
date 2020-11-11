e66b63875f2ef789da844674dba2c9cafb033a1a
virtual-man007 <centronic_solutions@hotmail.com>
2020-11-11 17:35:47 -0400

    # 2020-11-11 mass backup gamelist.xml for 66+ systems # 2020-11-11 clean_gamelist batch file by @bilu
    Signed-off-by: virtualman

---
+++ scripts/clean_gamelists.bat
@@ -0,0 +1,9 @@
+@echo off
+echo ** VMAN Retro-Bliss LevelUp! gamelist.xml cleaner v1.0 by Bilu **
+echo.
+echo Cleaning gamelist.xml statistics in all ROM folders... 
+powershell -Command "Invoke-Command -scriptblock {$ErrorActionPreference = 'SilentlyContinue';Get-Childitem V:\roms\gamelist.xml -depth 1 | ForEach {Set-Content -Path $_ -Value(get-content -Path $_ | Select-String -Pattern '^.*</playcount>$|^.*</lastplayed>|^.*</gametime>$' -NotMatch)}}"
+echo.
+echo Complete.
+echo.
+pause
