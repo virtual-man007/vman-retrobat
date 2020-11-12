2ec853732b6d4d5fd6a7bd2503986b7055c802e8
virtual-man007 <centronic_solutions@hotmail.com>
2020-11-11 21:22:48 -0400

    # 2020-11-11 added script fix_videosnaps by @bilu
    Signed-off-by: virtualman

---
+++ scripts/fix_videosnaps.bat
@@ -0,0 +1,26 @@
+@echo off
+cls
+echo ** VMAN Retro-Bliss LevelUp! videosnaps fixer v1.0 by Bilu **
+echo.
+echo Pre-Req you will need to extract V:_tools\ffmpeg\ffmpeg.7z prior to using this script.
+echo.
+echo Press Ctlr-C to exit, any other key to continue.
+pause
+echo Cleaning possible corrupt video snaps in all ROM folders... 
+echo.
+rem setlocal enableDelayedExpansion
+set ENC=V:\_tools\ffmpeg\ffmpeg.exe
+set TMPFILE=V:\temp.mp4
+set LOGFILE=V:\videosnaps_log.txt
+del /q %LOGFILE%
+cd V:\roms
+FOR /R %%I IN (snap\*.mp4) DO (
+ECHO %%I
+%ENC% -y -i "%%I" -c copy %TMPFILE% -loglevel error 2>> %LOGFILE%  
+move /y %TMPFILE% "%%I" 1> nul
+)
+echo.
+echo Complete.
+echo.
+:Abort
+pause
