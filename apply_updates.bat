f01ab01e6ba2dccb8f758f7bf820d24f8afd3112
virtual-man007 <centronic_solutions@hotmail.com>
2020-11-21 21:20:36 -0400

    # 2020-11-21 - Updated es_systems master files to support ports - vman-rbl-update update self-update package - apply_update.bat test first batch update es systems
    Signed-off-by: virtualman

--- apply_updates.bat
+++ apply_updates.bat
@@ -1,5 +1,9 @@
 echo Apply Updates Retro-Bliss LevelUp!
 pause
 REM ~VMAN-Retro-Bliss_LevelUP! 1.0 ~ Post-changes
-# 2020-11-05 - First fix :)...
-pause fix#1 applied!
+# 2020-11-21 - es_systems master cfg update for ports by @bilu
+cd V:\_tools\vman-retrobat-master\
+copy /y .\RetroBat\system\templates\emulationstation\*.cfg V:\RetroBat\system\templates\emulationstation\
+
+echo Update Completed
+pause
--- scripts/vman_rbl_updater.bat
+++ scripts/vman_rbl_updater.bat
@@ -1,2 +1,3 @@
-echo ** VMAN Retro-Bliss LevelUp! Updater v1.0 by Bilu **
+echo ** VMAN Retro-Bliss LevelUp! Updater v1.1 by Bilu **
 powershell -Command "Remove-Item -Path '.\vman-retrobat-master' -Recurse;Invoke-Command -scriptblock {wget https://github.com/virtual-man007/vman-retrobat/archive/master.zip -Outfile vman-retrobat.zip;Expand-Archive -Path 'vman-retrobat.zip' -DestinationPath '.';Remove-Item -Path 'vman-retrobat.zip'}"
+copy /y V:\_tools\vman-retrobat-master\scripts\vman_rbl_updater.bat V:\_tools
