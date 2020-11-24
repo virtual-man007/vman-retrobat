2942f35d3c1f522f774d4923e1be244609f135b5
virtual-man007 <centronic_solutions@hotmail.com>
2020-11-24 14:08:19 -0400

    # 2020-11-24 vman_rbl_updater and apply_updates batch 1.1 by @bilu
    Signed-off-by: virtualman

--- apply_updates.bat
+++ apply_updates.bat
@@ -1,9 +1,28 @@
-echo Apply Updates Retro-Bliss LevelUp!
-pause
-REM ~VMAN-Retro-Bliss_LevelUP! 1.0 ~ Post-changes
-# 2020-11-21 - es_systems master cfg update for ports by @bilu
-cd V:\_tools\vman-retrobat-master\
-copy /y .\RetroBat\system\templates\emulationstation\*.cfg V:\RetroBat\system\templates\emulationstation\
+@echo off
+echo.
+echo Apply Updates ~VMAN-Retro-Bliss_LevelUP! 1.0 ~ 
+echo Terminating running Retrobat processes...
+echo.
+TASKKILL /F /IM retrobat.exe /IM emulationstation.exe /IM retroarch.exe 2>nul
+DEL /F /Q %TEMP%\vman_update.tmp 2>nul
+echo Welcome to Virtualman Post-Fixes and Optimization Menu, By VMAN!
+echo This script will update and optimize common Retrobat files to any other associated files for VMAN's build.
+echo.
+echo Refer to VMAN's github repo for all changes at https://github.com/virtual-man007/vman-retropie
+echo You can run from cli vman_rbl_updater.bat and it will close Retrobat/Emulationstation automatically.
+echo.
+echo IMPORTANT! - I suggest you backup all your config files manually, however script will create backup of any existing files with extension of *.VM
+echo.
+echo Press any key to continue . . .
+pause >nul
 
-echo Update Completed
-pause
+rem #2020-11-21 - es_systems master cfg update for ports by @bilu
+rem cd V:\_tools\vman-retrobat-master\
+rem copy /y .\RetroBat\system\templates\emulationstation\*.cfg V:\RetroBat\system\templates\emulationstation\
+
+echo.
+echo Update Completed. Enjoy! :)
+echo.
+echo Press any key to continue . . .
+pause >nul
+ 
--- scripts/vman_rbl_updater.bat
+++ scripts/vman_rbl_updater.bat
@@ -1,3 +1,20 @@
-echo ** VMAN Retro-Bliss LevelUp! Updater v1.1 by Bilu **
+@echo off
+echo.
+set VERSION=** VMAN Retro-Bliss LevelUp! Updater v1.2 by Bilu **
+IF EXIST %TEMP%\vman_update.tmp (
+echo %VERSION%
+echo.
+echo Starting updates, logs available at V:\_tools 
+powershell -command "V:\_tools\vman-retrobat-master\apply_updates.bat 2>&1 | tee ('V:\_tools\vman-update-{0}.log' -f (Get-Date -format 'yyyyMMddHHmm'))"
+) ELSE (
+cls
+echo %VERSION%
+echo.
+echo Fetching latest updates...
+echo Updating script...
 powershell -Command "Remove-Item -Path '.\vman-retrobat-master' -Recurse;Invoke-Command -scriptblock {wget https://github.com/virtual-man007/vman-retrobat/archive/master.zip -Outfile vman-retrobat.zip;Expand-Archive -Path 'vman-retrobat.zip' -DestinationPath '.';Remove-Item -Path 'vman-retrobat.zip'}"
-copy /y V:\_tools\vman-retrobat-master\scripts\vman_rbl_updater.bat V:\_tools
+copy /y V:\_tools\vman-retrobat-master\scripts\vman_rbl_updater.bat V:\_tools > nul
+type NUL > %TEMP%\vman_update.tmp
+echo Script updated.
+V:\_tools\vman-retrobat-master\scripts\vman_rbl_updater.bat
+)
