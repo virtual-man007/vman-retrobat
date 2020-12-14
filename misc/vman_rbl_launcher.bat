1dd2d6884cade65b12f528615ae2d139e416e3a3
virtual-man007 <centronic_solutions@hotmail.com>
2020-12-14 19:37:53 -0400

    # 2020-12-14 vman_rbl_launcher noconfig switch and echo added
    Signed-off-by: virtualman

--- misc/vman_rbl_launcher.bat
+++ misc/vman_rbl_launcher.bat
@@ -1,1 +1,4 @@
-powershell -Command "gci "V:\RetroBat\emulationstation\.emulationstation\video\*" -include *.mp4 -recurse | Get-Random -Count 1 | ForEach { Start-Process -NoNewWindow  "V:\_tools\ffmpeg\mplayer.exe `-ontop` `-really-quiet` `-fs` `-ao` `sdl` $_" };Start-Sleep -s 42" & V:\RetroBat\retrobat.exe
+@echo off
+echo.
+echo Starting VMAN-Retro-Bliss_LevelUP! Enjoy! :)
+powershell -Command "gci "V:\RetroBat\emulationstation\.emulationstation\video\*" -include *.mp4 -recurse | Get-Random -Count 1 | ForEach { Start-Process -NoNewWindow  "V:\_tools\ffmpeg\mplayer.exe `-nofontconfig` `-ontop` `-really-quiet` `-fs` `-ao` `sdl` $_" };Start-Sleep -s 42; V:\RetroBat\retrobat.exe"
