fe0799600fca15bc24ee58812528d9e1cb568b70
virtual-man007 <centronic_solutions@hotmail.com>
2021-01-01 15:32:55 -0400

    # 2021-01-01 apply_update changed message output
    Signed-off-by: virtualman

--- apply_updates.bat
+++ apply_updates.bat
@@ -10,15 +10,16 @@ for /f "skip=3 eol=: delims=" %%F in ('dir /b /o-d vman-update-*.log') do @del "
 popd
 echo Welcome to Virtualman Post-Fixes and Optimization Menu, By VMAN!
 echo This script will update and optimize common Retrobat files to any other associated files for VMAN's build.
+echo IMPORTANT! - RetroBat/Emulationstation will close automatically during the update process.
 echo.
-echo Refer to VMAN's github repo for all changes at https://github.com/virtual-man007/vman-retropie
-echo You can run from cli vman_rbl_updater.bat and it will close Retrobat/Emulationstation automatically.
+echo Refer to VMAN's github repo for all changes at https://github.com/virtual-man007/vman-retrobat
+echo You also have option to run from cli v:\_tools\vman_rbl_updater.bat.
 echo.
-echo IMPORTANT! - I suggest you backup all your config files manually, however script will create backup of any existing files with extension of *.VM
+echo IMPORTANT! - Backup all your config files manually, however script will create backup of any existing files with extension of *.VM
 echo.
 echo Update details located in V:\_tools\vman-retrobat-master\updates.txt.
 echo.
-echo Press any key to continue . . .
+echo Press any key to continue to start the update process - be patient . . .
 pause >nul
 
 powershell -Command "(Get-Content 'V:\_tools\vman-retrobat-master\updates.txt') -notlike '#*' | ConvertFrom-Csv -Delimiter '|' | ForEach-Object {ForEach ($file in (Get-ChildItem -Path "$($_.Files)" -depth 0 -exclude *.VM)){Remove-Item -ErrorAction SilentlyContinue -Path $($file.fullname + '.VM');Copy-Item -Path $($file.fullname) -Destination $($file.fullname + '.VM');$filecontent = Get-Content -path $file ; if ($($_.LineNr)) {$filecontent[$($_.LineNr)] = $filecontent[$($_.LineNr)] -creplace $($_.SearchTxt),$($_.ReplaceTxt)} else {$filecontent = $filecontent -creplace $($_.SearchTxt),$($_.ReplaceTxt)} ; Set-Content $file.PSpath -Value $filecontent }}"
