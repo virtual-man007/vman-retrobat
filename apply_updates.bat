10946281c671527fc9f1286f1500eefe10f1b2db
virtual-man007 <centronic_solutions@hotmail.com>
2021-01-04 17:36:38 -0400

    # 2021-01-04 - Fix red smudges in Amiga and PS2 bezels - reported by @bilu
    Signed-off-by: virtualman

--- apply_updates.bat
+++ apply_updates.bat
@@ -17,12 +17,14 @@ echo You also have option to run from cli v:\_tools\vman_rbl_updater.bat.
 echo.
 echo IMPORTANT! - Backup all your config files manually, however script will create backup of any existing files with extension of *.VM
 echo.
-echo Update details located in V:\_tools\vman-retrobat-master\updates.txt.
-echo.
 echo Press any key to continue to start the update process - be patient . . .
 pause >nul
 
 powershell -Command "(Get-Content 'V:\_tools\vman-retrobat-master\updates.txt') -notlike '#*' | ConvertFrom-Csv -Delimiter '|' | ForEach-Object {ForEach ($file in (Get-ChildItem -Path "$($_.Files)" -depth 0 -exclude *.VM)){Remove-Item -ErrorAction SilentlyContinue -Path $($file.fullname + '.VM');Copy-Item -Path $($file.fullname) -Destination $($file.fullname + '.VM');$filecontent = Get-Content -path $file ; if ($($_.LineNr)) {$filecontent[$($_.LineNr)] = $filecontent[$($_.LineNr)] -creplace $($_.SearchTxt),$($_.ReplaceTxt)} else {$filecontent = $filecontent -creplace $($_.SearchTxt),$($_.ReplaceTxt)} ; Set-Content $file.PSpath -Value $filecontent }}"
+
+rem 5. 2021-01-04 - Fix red smudges in Amiga and PS2 bezels - reported by bilu
+copy /Y V:\RetroBat\system\decorations\default_unglazed\systems\amiga1200.png V:\RetroBat\decorations\default\systems\amiga.png
+copy /Y V:\RetroBat\system\decorations\default_unglazed\systems\ps2.png V:\RetroBat\decorations\default\systems\ps2.png
 
 echo.
 echo Update Completed. Enjoy! :)
