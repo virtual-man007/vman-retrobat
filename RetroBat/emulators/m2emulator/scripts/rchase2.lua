ac02dd0988e07d5d3bd939361fe8e8378fde6eea
virtual-man007 <centronic_solutions@hotmail.com>
2021-07-10 10:40:37 -0300

    # 2021-07-10 prep for future updates/backup emulator configs

---
+++ RetroBat/emulators/m2emulator/scripts/rchase2.lua
@@ -0,0 +1,13 @@
+require("model2");	-- Import model2 machine globals
+
+function Init()
+	
+end
+
+function Frame()
+	Model2_SetWideScreen(1)
+	Model2_SetStretchALow(1)
+	Model2_SetStretchAHigh(1)
+	Model2_SetStretchBLow(1)
+	Model2_SetStretchBHigh(1)
+end
